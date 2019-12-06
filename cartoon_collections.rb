9e3b2934bf714f907dd4571810363e9c5e474674
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-06 19:17:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,12 +15,16 @@ def roll_call_dwarves(dwarves)# code an argument here
     puts "#{index + 1}. #{dwarf}"
   end # Your code here
 end
-
 roll_call_dwarves(dwarves)
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+summon_the_captain = %w[earth wind fire water heart]
+
+def summon_captain_planet(summon_the_captain )# code an argument here
+  summon_the_captain.map{|conjure| conjure.capitalize + '!'}# Your code here
 end
+summon_captain_planet(summon_the_captain)
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
